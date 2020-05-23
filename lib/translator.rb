8f7bb50430eab91cc35740f60dade9354f13ebc6
Carly L <carlylynnla@gmail.com>
2020-05-23 21:25:31 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,7 +2,13 @@ require 'yaml'
 
 def load_library(file)
   emoticon_file = YAML.load_file(file)
-
+  organized_emoticon_file = {}
+    emoticon_file.each do |emoticons_in_array|
+      ['english', 'japanese'].zip(emoticons_in_array).to_h
+        
+      end
+    end
+  organized_emoticon_file
 end
 
 def get_japanese_emoticon

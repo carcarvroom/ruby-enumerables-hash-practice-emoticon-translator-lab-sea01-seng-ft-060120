dbd92e67fe4f8c52981d49c778928f4a99831770
Carly L <carlylynnla@gmail.com>
2020-05-23 23:03:46 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,5 +1,4 @@
 require 'yaml'
-require 'pry'
 
 def load_library(file)
   emoticon_file = YAML.load_file(file)
@@ -30,5 +29,4 @@ def get_english_meaning(file, japanese_emoticon)
     english_meaning = library.key(japanese_emoticon) 
     puts english_meaning
  end
- binding.pry
 end

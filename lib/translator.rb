a3157a22b6d8b8755d5a1559f3101076ec165884
Carly L <carlylynnla@gmail.com>
2020-05-23 19:41:18 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,8 @@
-# require modules here
+require 'yaml'
 
 def load_library
-  # code goes here
+  emoticon_file = YAML.load_file('emoticons.yml')
+  puts emoticon_file
 end
 
 def get_japanese_emoticon

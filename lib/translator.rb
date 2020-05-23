665b342b6129dada32ffc8568a9d4649b2b1b021
Carly L <carlylynnla@gmail.com>
2020-05-23 21:24:57 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,7 @@
 require 'yaml'
 
-def load_library('lib/emoticons.yml')
-  emoticon_file = YAML.load_file('lib/emoticons.yml')
+def load_library(file)
+  emoticon_file = YAML.load_file(file)
 
 end
 
